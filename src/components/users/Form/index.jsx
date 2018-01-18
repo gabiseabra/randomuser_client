6b8d1f35c405b680637b2cc7520ff1b2046763fd
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-17 23:21:26 -0200

    Fixed Form state

--- src/components/users/Form/index.jsx
+++ src/components/users/Form/index.jsx
@@ -1,35 +1,56 @@
 import React, { Component } from "react"
-import styled from "styled-components"
-import { TextField } from "material-ui"
+import { withStyles } from "material-ui/styles"
+import {
+  Paper,
+  TextField,
+  Button,
+  IconButton
+} from "material-ui"
 
-const Container = styled.div`
-  flex: 0 1 auto;
-`
+const Container = withStyles({
+  root: {
+    flex: "0 1 auto",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "20px 30px"
+  }
+})(Paper)
 
-const rng = () => (Date.now() * Math.random())
+const rng = () => (Date.now() * Math.random() * 10000).toString(36)
 
 export default class Form extends Component {
   state = {
     count: 1,
-    seed: ""
+    seed: rng()
   }
 
   onSubmit = () => this.props.onSubmit(this.state)
 
   onChange = e => this.setState({ [e.target.name]: e.target.value })
 
-  randomize = () => this.setState({ seed: rng().toString(36).substring(0, 5) })
+  randomize = () => this.setState({ seed: rng() })
 
   render() {
-    const { count, seed } = this.props
+    const { count, seed } = this.state
 
     return (
-      <Container>
+      <Container elevation={12}>
         <TextField
           type="text"
+          label="Seed"
           name="seed"
           value={seed}
           onChange={this.onChange} />
+        <TextField
+          type="number"
+          label="Amount"
+          name="count"
+          value={count}
+          onChange={this.onChange} />
+        <Button raised color="primary" onClick={this.onSubmit}>
+          Insert Users
+        </Button>
       </Container>
     )
   }
